918cc3a80adb956d512e40e3ce9520f4d495a94f
alagunandhini <alagunadhini@gmail.com>
2025-02-23 16:28:21 +0530

    navigation finished

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("move").addEventListener("click",function(){
+    
+    document.getElementById("sign-up").style.display="none";
+    document.getElementById("sign-up-btn").style.display="none";
+    document.getElementById("sign-up-text").style.display="none";
+    document.getElementById("sign-in").style.display="block";
+    document.getElementById("sign-in-btn").style.display="block";
+    document.getElementById("sign-in-text").style.display="block";
+
+
+})
+document.getElementById("move2").addEventListener("click",function(){
+    
+    document.getElementById("sign-up").style.display="block";
+    document.getElementById("sign-up-btn").style.display="block";
+    document.getElementById("sign-up-text").style.display="block";
+    document.getElementById("sign-in").style.display="none";
+    document.getElementById("sign-in-btn").style.display="none ";
+    document.getElementById("sign-in-text").style.display="none";
+
+
+
+})
+
